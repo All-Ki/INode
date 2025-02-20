aeeab698cf4ddd61096c7323ad26685db9635fee
Ki <k.allegrain@gmail.com>
2025-02-20 08:26:37 +0100

    initial upload

---
+++ src/lib/node/Node.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  class Node<TIn, TOut> {
+    public countIn: number = 3;
+    public countOut: number = 4;
+    public name: string = "node";
+    public apply(input: TIn): void {}
+  }
+
+  class NodeIoType {
+    public typeName: string = "type";
+    public value: any;
+  }
+
+  class NodeIoValue<T extends NodeIoType> {}
+
+  let n = new Node<number, number>();
+</script>
+
+<span>
+  <h1>{n.name}</h1>
+  <ul>
+    {#each { length: n.countIn }}
+      <li>itesm</li>
+    {/each}
+  </ul>
+
+  <ul>
+    {#each { length: n.countOut }}
+      <li>item</li>
+    {/each}
+  </ul>
+</span>
